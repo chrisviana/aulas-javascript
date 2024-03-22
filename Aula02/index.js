dbf0123a2a8fa0f9c365591b514df976cc74da83
Christian Viana <viana2015christian@gmail.com>
2024-03-21 21:26:17 -0300

    Aula de variaveis, console, alert, prompt

---
+++ Aula02/index.js
@@ -0,0 +1,70 @@
+//Declaração
+// var nome
+// var sobrenome
+// var idade
+
+// //atribuição
+// //@String
+// nome = "Pedro" 
+// sobrenome = 'Silva'
+// idade = 25
+
+var marca = "Adidas"
+console.log(marca)
+
+// + -  * / ** %
+
+var primeiroNumero = 1
+var segundoNumero = 2
+var terceiroNumero = 3
+
+//Soma de dois valores 
+var soma = primeiroNumero + segundoNumero
+console.log("Soma", soma)
+//Subtração de dois valores
+var subtracao =  segundoNumero - primeiroNumero
+console.log("Subtracao", subtracao)
+//Multiplicação de dois valores
+var multiplicacao = primeiroNumero * terceiroNumero
+
+//Divisão de dois valores
+var divisao = primeiroNumero / terceiroNumero
+
+//Resto da divisão
+var resto = primeiroNumero % terceiroNumero
+
+//Potencia
+var potencia = primeiroNumero ** terceiroNumero
+
+var textoA = "Coder"
+var textoB = "House"
+var textoC = " "
+
+//Concatenação
+var resultadoA = textoA + " " + textoB
+console.log(resultadoA)
+var resultadB = textoA + textoC + textoB
+console.log(resultadB)
+var resultadoC = textoA + 1
+console.log(resultadoC)
+
+var nome
+var sobrenome
+var anoAtual
+var idadeCompletada 
+
+nome = "Christian"
+sobrenome = "Viana"
+anoAtual = 2024
+idadeCompletada = 27
+
+var nomeCompleto = nome + " " + sobrenome
+var anoDeNascimento = anoAtual - idadeCompletada
+
+console.log(nomeCompleto)
+console.log(anoDeNascimento)
+
+var nomeInserido = prompt("Insira seu nome")
+console.log("Nome inserido: ", nomeInserido)
+
+alert("Seja bem vindo ao curso de JavaScript")
---
+++ Aula02/index1.js
@@ -0,0 +1,23 @@
+
+// var entrada = prompt("Insira uma letra")
+// var saida = entrada + " " + " info inserida"
+// alert(saida)
+
+// var valorString = par(prompt("Insira um numero"))
+// console.log(valorString + 1)
+// var valorConvertido = parseInt(valorString)
+// console.log(valorConvertido + 1)
+
+
+var numero1 = prompt("Valor 1")
+var numero2 = prompt("Valor 2")
+
+var numero1Convertido = parseFloat(numero1)
+var numero2Convertido = parseFloat(numero2)
+
+var soma = numero1Convertido + numero2Convertido
+console.log(soma)
+
+var produto = numero1Convertido * numero2Convertido
+alert("Produto" + produto)
+
